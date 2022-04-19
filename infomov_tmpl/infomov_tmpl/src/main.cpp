353521c2f94a7330ca068a28ff3ac58b53d832a5
Dionysi <dionysialex@gmail.com>
2022-04-19 14:40:40 +0200

    Reworked the entire template

---
+++ infomov_tmpl/infomov_tmpl/src/main.cpp
@@ -0,0 +1,41 @@
+#include "App.h"
+#include <imgui_impl_opengl3.h>
+#include <imgui_impl_glfw.h>
+
+class DemoApp : public App {
+
+public:
+	DemoApp(uint width, uint height) : App(width, height) { }
+
+protected:
+	void Tick(float dt) override {
+
+	}
+	void Draw(float dt) override {
+
+	}
+	void RenderGUI(float dt) override {
+		// feed inputs to dear imgui, start new frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		const static char* windowTitle = "Debug";
+		static bool display = true;
+		ImGui::Begin(windowTitle, &display, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
+		ImGui::SetWindowFontScale(1.5f);
+		ImGui::Text("Frame-time: %.1f", dt * 1000.0f);
+		ImGui::End();
+
+		// Render dear imgui into screen
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	}
+};
+
+int main() {
+	DemoApp* app = new DemoApp(1080, 720);
+	app->Run();
+
+	return 0;
+}
--- ocl_tmpl/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "application.h"
-
-int main() {
-
-	Application app = Application(1920, 1088);
-	app.Run();
-
-	return -1;
-}
